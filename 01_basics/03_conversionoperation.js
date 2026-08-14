73a459e9b18f13484b5be40a70e862a9cc38b546
ShaibaazKarbelkar <shaibaazk890@gmail.com>
2026-08-15 00:41:05 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valuInNumber);
+// console.log(valueInNumber);
+
+
+// "33" > 33
+// "33ab" > NaN
+// true > 1; false > 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = String(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
