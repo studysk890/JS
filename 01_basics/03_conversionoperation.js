aeb301ba88a919898c3e65269a39f4d6a3bbfdcb
ShaibaazKarbelkar <shaibaazk890@gmail.com>
2026-08-15 03:33:13 +0530

    datatypes summary

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -23,3 +23,29 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
+
+let value = 9
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Shaibaaz"
+let str3 = str1 + str2;
+console.log(str3)
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
